5ac59de49b9481f07046e1d3705bb6443cf57819
vladimir-zlatev <125951708+vladimir-zlatev@users.noreply.github.com>
2023-04-30 22:49:41 -0700

    Add Fast Food 1
    Add Fast Food 1

---
+++ lists_as_stacks_and_queues_exercise/fast_food_1.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+total_food = int(input())
+
+orders = deque(int(x) for x in input().split(" "))
+
+print(max(orders))
+
+while orders:
+    current_order = orders[0]
+
+    if current_order <= total_food:
+        orders.popleft()
+        total_food -= current_order
+    else:
+        break
+
+if orders:
+    print("Orders left: " + " ".join(str(x) for x in orders))
+else:
+    print("Orders complete")
